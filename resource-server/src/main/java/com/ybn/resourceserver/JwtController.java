a54911a956415436e1c5bc84118d040696d4839f
Amazigh Djebarri <amazigh.djebarri@capgemini.com>
2020-06-28 13:50:45 +0200

    refresh token

--- resource-server/src/main/java/com/ybn/resourceserver/JwtController.java
+++ resource-server/src/main/java/com/ybn/resourceserver/JwtController.java
@@ -5,10 +5,7 @@ import com.ybn.common.dto.TicketAuthDto;
 import com.ybn.resourceserver.service.JwtServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/jwt")
@@ -17,6 +14,11 @@ public class JwtController {
     private JwtServiceImpl jwtService;
     @PostMapping("/generate")
     public ResponseEntity<OAuth2TokenDto> produce(@RequestBody  TicketAuthDto body) {
-        return this.jwtService.produce(body.getUsername(), body.getPassword(), "password", "/oauth/token");
+        return this.jwtService.produce(body.getUsername(), body.getPassword(), "/oauth/token");
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<OAuth2TokenDto> refresh(@RequestParam("refresh_token") String refresh) {
+        return this.jwtService.produce(refresh, "/oauth/token");
     }
 }
--- resource-server/src/main/java/com/ybn/resourceserver/service/JwtServiceImpl.java
+++ resource-server/src/main/java/com/ybn/resourceserver/service/JwtServiceImpl.java
@@ -19,11 +19,23 @@ public class JwtServiceImpl {
     @Autowired
     private AppConfig config;
 
-    public ResponseEntity<OAuth2TokenDto> produce(String username, String password, String grant, String endpoint) {
+    public ResponseEntity<OAuth2TokenDto> produce(String username, String password, String endpoint) {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("username", username);
         body.add("password", password);
-        body.add("grant_type", grant);
+        body.add("grant_type", "password");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.setBasicAuth(this.config.getClientId(), this.config.getClientSecret());
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity(body, headers);
+        String url = new StringBuilder(config.getRunningOn()).append(endpoint).toString();
+        return this.restTemplate.postForEntity(url, entity, OAuth2TokenDto.class);
+    }
+
+    public ResponseEntity<OAuth2TokenDto> produce(String token, String endpoint) {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("refresh_token", token);
+        body.add("grant_type", "refresh_token");
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         headers.setBasicAuth(this.config.getClientId(), this.config.getClientSecret());
